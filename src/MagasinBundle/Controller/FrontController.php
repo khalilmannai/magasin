b87e420effc5341b106ebe08dab23109ca5e64ec
khalilmannai <khalilmannai@users.noreply.github.com>
2018-03-02 17:40:26 +0100

    khalil

--- src/MagasinBundle/Controller/FrontController.php
+++ src/MagasinBundle/Controller/FrontController.php
@@ -13,15 +13,37 @@ class FrontController extends Controller
     {
         $em= $this->getDoctrine()->getRepository('MagasinBundle\Entity\Categorie') ;
         $cat=$em->findAll();
+        if(isset($_SESSION['login'])){
+            return $this->render('MagasinBundle:Front:index.html.twig',array('cat'=> $cat,"name"=>$_SESSION['login']));
+        }
         return $this->render('MagasinBundle:Front:index.html.twig',array('cat'=> $cat));
     }
 
 
     public function connexionAction()
     {
-        $em= $this->getDoctrine()->getRepository('MagasinBundle\Entity\User') ;
+        if (!isset($_SESSION))
+        {
+            session_start();
+        }
+        session_destroy();
+        if($_POST){
+            session_start();
+            $em= $this->getDoctrine()->getRepository('MagasinBundle\Entity\User');
+            $user=$em->findOneBy(array('email'=>$_POST['Email'],'password'=>$_POST['Password']));
+            if($user){
+                $login=$user->getLogin();
+                $_SESSION['login']=$login;
+                return $this->render('MagasinBundle:Front:index.html.twig',array("name"=>$_SESSION['login']));
+
+            }else{
+                return $this->render('MagasinBundle:Front:connexion.html.twig',array("error"=>" login Incorrect !"));
+            }
+        }
+        $em= $this->getDoctrine()->getRepository('MagasinBundle\Entity\Categorie') ;
         $cat=$em->findAll();
-        return $this->render('MagasinBundle:Front:connexion.html.twig');
+        return $this->render('MagasinBundle:Front:connexion.html.twig',array("cat"=>$cat));
+
     }
 
 }
