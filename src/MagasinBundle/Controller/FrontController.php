98686c7f7222c421bcde25a5e7ffec085db06f2b
khalilmannai <khalilmannai@users.noreply.github.com>
2018-03-06 15:18:02 +0100

    khalil

--- src/MagasinBundle/Controller/FrontController.php
+++ src/MagasinBundle/Controller/FrontController.php
@@ -46,6 +46,31 @@ class FrontController extends Controller
 
     }
 
+
+
+    public function modifierprofileAction($id)
+    {
+        if ($_POST) {
+            $em = $this->getDoctrine()->getManager();
+            $profile = $em->getRepository('MagasinBundle\Entity\User')->find($id);
+            $profile->setNom($_POST['nom']);
+            $profile->setPrenom($_POST['prenom']);
+            $profile->setEmail($_POST['email']);
+            $profile->setTel($_POST['tel']);
+            $profile->setAdresse($_POST['adresse']);
+            $profile->setPseudo($_POST['pseudo']);
+            $profile->setLogin($_POST['login']);
+            $profile->setPassword($_POST['password']);
+            $em->flush();
+            return $this->render('MagasinBundle:Front:modifierprofile.html.twig',array("name"=>$_SESSION['login']));
+        }
+
+        $ems = $this->getDoctrine()->getRepository('MagasinBundle\Entity\Categorie');
+        $cat = $ems->findAll();
+        return $this->render('MagasinBundle:Front:modifierprofile.html.twig',array("cat"=>$cat,"name"=>$_SESSION['login']));
+
+    }
+
 }
 
 
