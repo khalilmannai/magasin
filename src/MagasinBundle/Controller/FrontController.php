31b6d68b27a8bc4bd6c6b662792fa74b077d1478
khalilmannai <khalilmannai@users.noreply.github.com>
2018-03-06 17:35:18 +0100

    khalil

--- src/MagasinBundle/Controller/FrontController.php
+++ src/MagasinBundle/Controller/FrontController.php
@@ -4,6 +4,7 @@ namespace MagasinBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use MagasinBundle\Entity\Categorie;
+use MagasinBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 
@@ -58,7 +59,7 @@ class FrontController extends Controller
             $profile->setEmail($_POST['email']);
             $profile->setTel($_POST['tel']);
             $profile->setAdresse($_POST['adresse']);
-            $profile->setPseudo($_POST['pseudo']);
+            $profile->setType($_POST['type']);
             $profile->setLogin($_POST['login']);
             $profile->setPassword($_POST['password']);
             $em->flush();
@@ -74,6 +75,29 @@ class FrontController extends Controller
 
     public function inscriptionAction()
     {
+        if($_POST){
+            $em=$this->getDoctrine()->getManager();
+            $user= new User();
+            $user->setNom($_POST['nom']);
+            $user->setPrenom($_POST['prenom']);
+            $user->setAdresse($_POST['adresse']);
+            $user->setTel($_POST['tel']);
+            $user->setEmail($_POST['email']);
+            $user->setLogin($_POST['login']);
+            $user->setPassword($_POST['password']);
+            $user->setType($_POST['type']);
+            $ema= $this->getDoctrine()->getRepository('MagasinBundle\Entity\Categorie') ;
+            $cat=$ema->findAll();
+            $ems= $this->getDoctrine()->getRepository('MagasinBundle\Entity\User') ;
+            $us= $ems->findOneBy(array('email'=>$_POST['email']));
+            if($us){
+                return $this->render('MagasinBundle:Front:inscription.html.twig',array('cat'=> $cat,'error'=>"Cette adresse e-mail est déjà utilisé. Essayez un autre adresse.!"));
+            }else{
+                $em->persist($user);
+                $em->flush();
+                return $this->render('MagasinBundle:Front:inscription.html.twig',array('cat'=> $cat,'ok'=>"inscription effectuée, vous pouvez se connecter ."));
+            }
+        }
         $em= $this->getDoctrine()->getRepository('MagasinBundle\Entity\Categorie') ;
         $cat=$em->findAll();
         return $this->render('MagasinBundle:Front:inscription.html.twig',array("cat"=>$cat));
